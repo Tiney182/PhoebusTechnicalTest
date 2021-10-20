8aa6850f76d09812abc249710fb79770ab4b3df9
Andrew Brightman <amb@phoebussoftware.com>
2021-10-20 09:37:57 +0100

    Updating rest endpoints to not be ambiguous

--- src/main/java/com/phoebussoftware/technicalTest/web/AccountController.java
+++ src/main/java/com/phoebussoftware/technicalTest/web/AccountController.java
@@ -4,9 +4,11 @@ import com.phoebussoftware.technicalTest.DTO.AccountDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/account")
 public class AccountController {
 
   @GetMapping("/{accountId}")
